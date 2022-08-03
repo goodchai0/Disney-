0219df6932c1771e27c24ad45f42131a5d111ae0
Amit <www.amitkewot@gmail.com>
2022-08-03 16:06:35 +0530

    sign-out & slider added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
+import Home from './components/Home'
 import Header from './components/Header'
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header/>
         <Routes>
           <Route exact path='/' element={<Login/>} />
+          <Route exact path='/home' element={<Home/>} />
         </Routes>
       </Router>
     </div>
